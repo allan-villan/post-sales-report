2810fbb78f4f4b10e380b134eaa4bb448d83026c
allan <allandv96@gmail.com>
2022-07-31 15:10:37 -0700

    Small changes to comments

--- autoinc_spark.py
+++ autoinc_spark.py
@@ -9,15 +9,15 @@
     return a tuple containing the record's vin_number and necessary information.
 
     Arguments:
-        Line - data read from data.csv
+        input - data read from data.csv
 
     Returns:
         Tuple - key: vin number
                 value: tuple of (make, year, and incident type)
 
     """
-    def extract_vin_key_value(line: str):
-        values = line.split(",") # reads in data from data.csv
+    def extract_vin_key_value(input: str):
+        values = input.split(",") # reads in data from data.csv
         incident_type = values[1]
         vin_num = values[2]
         make = values[3]
@@ -25,13 +25,15 @@
         PairRDD = (make, year, incident_type)
         return (vin_num, PairRDD)
 
-
+    # Initialize SparkContext to use Spark Cluster
     sc = SparkContext("local", "My Application")
     raw_rdd = sc.textFile("data.csv")
+
+    # Creates key value pairs we can work with
     vin_kv = raw_rdd.map(lambda x: extract_vin_key_value(x))
 
     # Only records from an Initial Sale have make and year information
-    # Groups by key to populate make and year to all records using flatMap
+    # This line populates make and year to all records
     enhance_make = vin_kv.groupByKey()\
                         .flatMap(lambda kv: kv[1])\
                         .filter(lambda x: len(x[1]) > 0 and len(x[2]) > 0)\
@@ -46,7 +48,8 @@
     # Entry point to create a dataframe and utilize SQL queries
     spark = SparkSession.builder.appName('autoinc_spark').getOrCreate()
 
-    columns = ['make_year','count']
+    # Converts RDD to data frame so we can organize data using SQL
+    columns = ['make_year','count'] # Schema for the data frame
     df = make_kv_count.toDF(columns)
 
     df.sort(col("count").desc(), col("make_year").asc()).show()
